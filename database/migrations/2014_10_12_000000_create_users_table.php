1068a00c63f843a45f6ff5cb7c62912f4fb5df56
Kailash-Dholi <118329947+Kailash-Dholi@users.noreply.github.com>
2023-08-29 12:19:39 +0530

    Updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,6 +23,8 @@ class CreateUsersTable extends Migration
             $table->foreignId('current_team_id')->nullable();
             $table->text('profile_photo_path')->nullable();
             $table->timestamps();
+            $table->text('plain_text')->nullable();
+            $table->enum('deleted',['1','0'])->default('0');
         });
     }
 
